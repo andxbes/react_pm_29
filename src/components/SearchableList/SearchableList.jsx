a862c76d96b62164770e8f8b8a515581eea7e00d
andxbes <andxbes@gmail.com>
2025-05-01 22:10:05 +0300

    548

--- src/components/SearchableList/SearchableList.jsx
+++ src/components/SearchableList/SearchableList.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function SearchableList({ items }) {
+export default function SearchableList({ items, itemKeyFn, children }) {
     const [searchTerm, setSearchTerm] = useState('');
 
 
@@ -16,8 +16,8 @@ export default function SearchableList({ items }) {
         <div className="searchable-list">
             <input onChange={handleSearch} type="search" placeholder="search" />
             <ul>
-                {searchResults.map((item, index) => (
-                    <li key={index}>{item.toString()}</li>
+                {searchResults.map((item) => (
+                    <li key={itemKeyFn(item)}>{children(item)}</li>
                 ))}
             </ul>
         </div>
